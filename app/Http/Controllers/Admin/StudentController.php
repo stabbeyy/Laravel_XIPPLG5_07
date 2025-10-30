65f7fa6f6a5c97205b3d73e449b43d373e34a4bd
stabbeyy <fbiadeshyaann@gmail.com>
2025-10-30 14:51:21 +0700

    Add Feature Edit Student

--- app/Http/Controllers/Admin/StudentController.php
+++ app/Http/Controllers/Admin/StudentController.php
@@ -22,7 +22,7 @@ class StudentController extends Controller
      */
     public function create()
     {
-        //
+        return view('admin.student.create');
     }
 
     /**
@@ -30,7 +30,16 @@ class StudentController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $request->validate([
+            'nis' => 'required|unique:students',
+            'nama_lengkap' => 'required',
+            'jenis_kelamin' => 'required',
+            'nisn' => 'required|unique:students',
+        ]);
+
+        student::create($request->all());
+        return redirect()->route('admin.students.index')
+                         ->with('success', 'Data berhasil disimpan!');
     }
 
     /**
